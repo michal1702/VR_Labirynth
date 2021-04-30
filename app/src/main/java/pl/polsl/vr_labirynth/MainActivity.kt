1292de484178a9043453a06c71c81da1a9e396ee
michal1702 <michalopielka17@gmail.com>
2021-04-30 11:37:13 +0200

    initial commit

---
+++ app/src/main/java/pl/polsl/vr_labirynth/MainActivity.kt
@@ -0,0 +1,11 @@
+package pl.polsl.vr_labirynth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
