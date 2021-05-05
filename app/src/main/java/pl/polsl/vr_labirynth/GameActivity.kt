5ff6abf1a0329e146986aaa272ccd06fdf875a4c
michal1702 <michalopielka17@gmail.com>
2021-05-05 10:40:19 +0200

    enabled javascript

--- app/src/main/java/pl/polsl/vr_labirynth/GameActivity.kt
+++ app/src/main/java/pl/polsl/vr_labirynth/GameActivity.kt
@@ -1,19 +1,25 @@
 package pl.polsl.vr_labirynth
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.webkit.WebChromeClient
 import pl.polsl.vr_labirynth.databinding.ActivityGameBinding
 
 class GameActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityGameBinding
 
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityGameBinding.inflate(layoutInflater)
         setContentView(binding.root)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        val webSettings = binding.gameWebView.settings
+        webSettings.javaScriptEnabled = true
+        binding.gameWebView.webChromeClient = WebChromeClient()
         binding.gameWebView.loadUrl("file:///android_asset/index.html")
     }
 }
