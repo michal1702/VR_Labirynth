a4b894e6cd6dd65c6aebe452868ff055b4891665
michal1702 <michalopielka17@gmail.com>
2021-05-05 10:38:35 +0200

    test html vr

---
+++ app/src/main/java/pl/polsl/vr_labirynth/HighScoresActivity.kt
@@ -0,0 +1,11 @@
+package pl.polsl.vr_labirynth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HighScoresActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_high_scores)
+    }
+}
